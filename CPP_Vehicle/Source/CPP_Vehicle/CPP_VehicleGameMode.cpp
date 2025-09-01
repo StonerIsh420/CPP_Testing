9489cbe1c1aad08f52f15a7edfb03c062121acc8
StonerIsh420 <130602477+StonerIsh420@users.noreply.github.com>
2025-08-31 20:46:10 -0500

    Delete CPP_Vehicle directory

--- CPP_Vehicle/Source/CPP_Vehicle/CPP_VehicleGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPP_VehicleGameMode.h"
-#include "CPP_VehiclePlayerController.h"
-
-ACPP_VehicleGameMode::ACPP_VehicleGameMode()
-{
-	PlayerControllerClass = ACPP_VehiclePlayerController::StaticClass();
-}
